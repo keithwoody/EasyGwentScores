512c3c2e42f2ee3aef23ecee3da516081d99eef6
Keith Woody <keith.woody@gmail.com>
2017-03-07 14:19:16 -0800

    add and extend faction.cards association

--- easy_gwent_scores/app/models/faction.rb
+++ easy_gwent_scores/app/models/faction.rb
@@ -2,6 +2,40 @@ require 'open-uri'
 class Faction < ApplicationRecord
   WIKI_BASE = 'http://witcher.wikia.com'
 
+  has_many :cards do
+    def import_from_wiki
+      faction = proxy_association.owner
+      columns = Card.content_columns.map(&:name).slice(0,8)
+      doc = Nokogiri::HTML(open(WIKI_BASE+faction.wiki_path))
+      doc.css('div#mw-content-text table tr')[1..-1].map do |row|
+        values = row.css('td')[1..7].map do |td|
+                  val = if td.css('a.image').any?
+                           td.css('a.image').attr('title').value
+                         else
+                           # sub to get rid of 0 from hidden div
+                           td.text.strip.sub(/^0n/, "n").sub(/0N/, 'N')
+                         end
+               end
+        if values[0] =~ /([\w\s]+) \((\d)\)/
+          values[0] = [$1,$2.to_i]
+        else
+          values[0] = [values[0], 1]
+        end
+        card = Hash[columns.zip(values.flatten)]
+        if card['num_related'] > 1
+          name = card['name']
+          1.upto(card['num_related']) do |n|
+            card['name'] = name + " #{n}/#{card['num_related']}"
+            self.create( card ) unless self.where(name: card['name']).exists?
+          end
+        else
+          self.create( card ) unless self.where(name: card['name']).exists?
+        end
+      end
+      count
+    end
+  end
+
   def self.import_from_wiki
     doc = Nokogiri::HTML(open(WIKI_BASE+'/wiki/Gwent'))
     doc.css('div#mw-content-text ul:first-of-type li').map do |e|
