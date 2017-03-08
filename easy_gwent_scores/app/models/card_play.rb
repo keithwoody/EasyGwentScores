c98c1910251c194d8700d56975c187197bfa2d0f
Keith Woody <keith.woody@gmail.com>
2017-03-08 12:22:03 -0800

    default card play side to board row, but allow override

--- easy_gwent_scores/app/models/card_play.rb
+++ easy_gwent_scores/app/models/card_play.rb
@@ -2,4 +2,10 @@ class CardPlay < ApplicationRecord
   belongs_to :board_side
   belongs_to :board_row
   belongs_to :card
+
+  before_validation :set_board_side, on: :create
+
+  def set_board_side
+    self.board_side = board_row.board_side unless board_side
+  end
 end
