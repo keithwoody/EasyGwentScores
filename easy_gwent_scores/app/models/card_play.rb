6c3e625318140d0501cb1a3b3af1fb748c4e2533
Keith Woody <keith.woody@gmail.com>
2017-03-08 15:47:53 -0800

    update card_play to allow global cards to be played by a side

--- easy_gwent_scores/app/models/card_play.rb
+++ easy_gwent_scores/app/models/card_play.rb
@@ -1,11 +1,13 @@
 class CardPlay < ApplicationRecord
   belongs_to :board_side
-  belongs_to :board_row
+  belongs_to :board_row, required: false
   belongs_to :card
 
   before_validation :set_board_side, on: :create
 
   def set_board_side
-    self.board_side = board_row.board_side unless board_side
+    if board_side.nil?
+      self.board_side = board_row.board_side if board_row
+    end
   end
 end
