2030b3bff5ccc8494d4bd6077591a3b0177ea95e
Keith Woody <keith.woody@gmail.com>
2017-03-08 16:05:12 -0800

    * when weather card is played, update affected rows weather_active flag

--- easy_gwent_scores/app/models/board_side.rb
+++ easy_gwent_scores/app/models/board_side.rb
@@ -6,11 +6,35 @@ class BoardSide < ApplicationRecord
   has_and_belongs_to_many :discarded_cards, class_name: 'Card'
 
   has_many :board_rows
-
   after_create do
     board_rows.melee.create!
     board_rows.ranged.create!
     board_rows.siege.create!
+  end
+
+  has_many :global_card_plays, ->{ where(board_row_id: nil) }, class_name: 'CardPlay',  after_add: :apply_weather
+
+  def apply_weather(card_play)
+    if card_play.card.weather?
+      round.side_one.apply_row_weather( card_play.card )
+      round.side_two.apply_row_weather( card_play.card )
+    end
+  end
+
+  def apply_row_weather( card )
+    name = card.name
+    if name =~ /Frost/
+      melee_row.update(weather_active: true)
+    elsif name =~ /Fog/
+      ranged_row.update(weather_active: true)
+    elsif name =~ /Rain/
+      siege_row.update(weather_active: true)
+    elsif name =~ /Storm/
+      ranged_row.update(weather_active: true)
+      siege_row.update(weather_active: true)
+    elsif name =~ /Clear/
+      board_rows.update(weather_active: false)
+    end
   end
 
   def melee_row
