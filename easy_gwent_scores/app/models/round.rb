9c12c95b59b7889d3e41de5d035348dbaa9acf15
Keith Woody <keith.woody@gmail.com>
2017-03-16 11:54:46 -0700

    FIXUP run twice typo

--- easy_gwent_scores/app/models/round.rb
+++ easy_gwent_scores/app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ApplicationRecord
   has_many :board_sides, -> { includes(:board_rows).order(:created_at) }, dependent: :destroy, inverse_of: :round
 
   after_create do
-    2.time do
+    2.times do
       board_sides.create!
     end
   end
