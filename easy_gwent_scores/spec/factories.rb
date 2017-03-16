8ae8758e5d8e69a5a076ea1a0f8a45737c370fc5
Keith Woody <keith.woody@gmail.com>
2017-03-16 12:53:41 -0700

    Add factories for Card and Faction

--- easy_gwent_scores/spec/factories.rb
+++ easy_gwent_scores/spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+  factory :card do
+    faction
+    sequence(:name) {|n| "Card #{n}" }
+  end
+  factory :faction do
+    sequence(:name) {|n| "Faction #{n}" }
+  end
   factory :round do
   end
   factory :board_side do
