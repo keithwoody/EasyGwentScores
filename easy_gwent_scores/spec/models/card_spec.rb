6fdd58d4f4c5bae6ccdddbb820cf7ebaa181210a
Keith Woody <keith.woody@gmail.com>
2017-03-21 11:29:30 -0700

    Refactor card specs to use unit and hero factories
    Add specs for morale boost in row

--- easy_gwent_scores/spec/models/card_spec.rb
+++ easy_gwent_scores/spec/models/card_spec.rb
@@ -13,37 +13,51 @@ RSpec.describe Card, type: :model do
       expect( subject.reload.strength ).to eq 0
     end
   end
-  describe "#row_score" do
-    subject { create(:card, strength: 0) }
+  describe "#row_score(row)" do
+    subject { create(:unit, strength: 5) }
+    let(:hero) { create(:hero, strength: 10) }
+    let(:row) { build(:board_row) }
     it "defaults to value of strength" do
-      expect( subject.row_score(nil) ).to eq subject.strength
-    end
-    it "leaves Hero strength unaffected" do
-      subject.update(card_type: 'Hero', strength: 2)
-      expect( subject.row_score(row) ).to eq 2
+      expect( subject.row_score(row) ).to eq subject.strength
     end
 
     context "when weather effects are active" do
       let(:row) { build(:board_row, weather_active: true) }
       it "leaves cards with 0 strength unchanged" do
-        expect( subject.strength ).to eq 0
+        subject.strength = 0
         expect( subject.row_score( row ) ).to eq 0
       end
       it "reduces Unit strength to 1" do
         subject.update(card_type: 'Unit', strength: 2)
         expect( subject.row_score(row) ).to eq 1
       end
+      it "leaves Hero strength unaffected" do
+        expect( hero.row_score(row) ).to eq hero.strength
+      end
+    end
+
+    context "when Morale Boost ability is present" do
+      it "adds boost count to each unit" do
+        allow(row).to receive(:morale_boosts_for).with(subject).and_return(1)
+        expect( subject.row_score(row) ).to eq( subject.strength + 1 )
+      end
+      it "leaves Hero strength unaffected" do
+        expect( hero.row_score(row) ).to eq hero.strength
+      end
     end
 
     context "when commander's horn is active" do
       let(:row) { build(:board_row, commanders_horn_active: true) }
       it "leaves cards with 0 strength unchanged" do
-        expect( subject.strength ).to eq 0
+        subject.strength = 0
         expect( subject.row_score( row ) ).to eq 0
       end
       it "doubles Unit strength" do
         subject.update(card_type: 'Unit', strength: 2)
         expect( subject.row_score(row) ).to eq 4
+      end
+      it "leaves Hero strength unaffected" do
+        expect( hero.row_score(row) ).to eq hero.strength
       end
     end
   end
