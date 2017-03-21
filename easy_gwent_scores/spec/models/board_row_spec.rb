60027baf37a8d8d322b1f94aebc23043b039032f
Keith Woody <keith.woody@gmail.com>
2017-03-21 15:06:54 -0700

    Add pending specs for BoardRow score helpers

--- easy_gwent_scores/spec/models/board_row_spec.rb
+++ easy_gwent_scores/spec/models/board_row_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe BoardRow, type: :model do
   it { is_expected.to validate_uniqueness_of(:combat_type).scoped_to(:board_side_id) }
   it { is_expected.to validate_inclusion_of(:combat_type).in_array(BoardRow::COMBAT_TYPES) }
 
+  describe "Callbacks" do
+    describe "after_update" do
+      it "updates the score for the associated BoardSide when the row score changes"
+    end
+  end
+
+  describe "#morale_boosts_for( card )" do
+    it "returns a count of morale cards in the row for non-morale cards"
+    it "returns a decremented count of morale cards in the row for morale cards"
+  end
+
+  describe "#tight_bonds_for( card )" do
+    it "returns 0 for cards that don't have the Tight Bond ability"
+    it "returns a count of related Tight Bond cards in the row"
+  end
 end
