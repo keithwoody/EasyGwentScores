cc87474bd69cf72b492da0f682e3c2559ec64a01
Keith Woody <keith.woody@gmail.com>
2017-03-21 14:54:18 -0700

    Remove callback to set board_side if not already set
    - assume card_plays will be created from board_side context
    Make BoardSide structure specs use consistent syntax

--- easy_gwent_scores/app/models/card_play.rb
+++ easy_gwent_scores/app/models/card_play.rb
@@ -3,7 +3,6 @@ class CardPlay < ApplicationRecord
   belongs_to :board_row, required: false
   belongs_to :card
 
-  before_validation :set_board_side, on: :create
 
   after_create do
     if board_row
@@ -11,9 +10,5 @@ class CardPlay < ApplicationRecord
     end
   end
 
-  def set_board_side
-    if board_side.nil?
-      self.board_side = board_row.board_side if board_row
-    end
   end
 end
