a5cd5197a3282f50c25b0c4e06270640000da834
Keith Woody <keith.woody@gmail.com>
2017-03-21 14:56:07 -0700

    Check for CardPlay#board_row_id if card requires it for valid play

--- easy_gwent_scores/app/models/card_play.rb
+++ easy_gwent_scores/app/models/card_play.rb
@@ -3,6 +3,7 @@ class CardPlay < ApplicationRecord
   belongs_to :board_row, required: false
   belongs_to :card
 
+  validates :board_row_id, presence: true, if: :card_requires_combat_row?
 
   after_create do
     if board_row
@@ -10,5 +11,10 @@ class CardPlay < ApplicationRecord
     end
   end
 
+  protected
+
+  def card_requires_combat_row?
+    card && !card.whole_board?
   end
+
 end
