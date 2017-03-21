691714959e4e479c30f414b6607ef278e0c9a6f4
Keith Woody <keith.woody@gmail.com>
2017-03-20 17:45:00 -0700

    Add factories for Hero and Unit cards

--- easy_gwent_scores/spec/factories.rb
+++ easy_gwent_scores/spec/factories.rb
@@ -2,6 +2,12 @@ FactoryGirl.define do
   factory :card do
     faction
     sequence(:name) {|n| "Card #{n}" }
+    factory :hero do
+      card_type 'Hero'
+    end
+    factory :unit do
+      card_type 'Unit'
+    end
   end
   factory :faction do
     sequence(:name) {|n| "Faction #{n}" }
