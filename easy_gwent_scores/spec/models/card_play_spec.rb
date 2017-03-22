dd54e5104792dc82bb1c5c10f2c9710f53109a32
Keith Woody <keith.woody@gmail.com>
2017-03-22 16:29:07 -0700

    DRY up card_play specs with row let definitions and create_play helper method
    Add specs to confirm weather status changes affect row score

--- easy_gwent_scores/spec/models/card_play_spec.rb
+++ easy_gwent_scores/spec/models/card_play_spec.rb
@@ -6,19 +6,48 @@ RSpec.describe CardPlay, type: :model do
   it { is_expected.to belong_to(:card) }
 
   let(:side_one) { create(:board_side) }
-  let(:row) { side_one.melee_row }
+  let(:melee_row) { side_one.melee_row }
+  let(:ranged_row) { side_one.ranged_row }
+  let(:siege_row) { side_one.siege_row }
   let(:unit5) { create(:unit, strength: 5) }
+  let(:hero10) { create(:hero, strength: 10) }
   let(:frost) { create(:frost) }
   let(:fog) { create(:fog) }
   let(:rain) { create(:rain) }
   let(:storm) { create(:storm) }
   let(:clear) { create(:clear_weather) }
+  def create_play( card, row )
+    side_one.card_plays.create(board_row: row, card: card)
+  end
   describe "Callbacks:" do
     describe "after_create" do
-      let(:row_play) { build(:card_play, board_side: side_one, board_row: row, card: unit5) }
-      it "updates the row's score if it was affected by the play" do
-        expect{ row_play.save }.to change{ row.reload.score }.from(0).to(5)
+      it "updates the row's score when a unit is played" do
+        expect{ create_play(unit5, melee_row) }.to change{ melee_row.score }.from(0).to(5)
       end
+      it "updates the row's score when a hero is played" do
+        expect{ create_play(hero10, melee_row) }.to change{ melee_row.score }.from(0).to(10)
+      end
+
+      it "updates the melee row's unit scores when a frost is played" do
+        create_play( unit5, melee_row )
+        expect{ create_play(frost, nil) }.to change{ melee_row.score }.from(5).to(1)
+      end
+      it "updates the ranged row's unit scores when a fog is played" do
+        create_play( unit5, ranged_row )
+        expect{ create_play(fog, nil) }.to change{ ranged_row.score }.from(5).to(1)
+      end
+      it "updates the siege row's unit scores when a rain is played" do
+        create_play( unit5, siege_row )
+        expect{ create_play(rain, nil) }.to change{ siege_row.score }.from(5).to(1)
+      end
+      it "updates the ranged and siege rows' unit scores when a storm is played" do
+        create_play( unit5, ranged_row )
+        create_play( unit5, siege_row )
+        expect{ create_play(storm, nil) }.to change{
+          ranged_row.score + siege_row.score
+        }.from(10).to(2)
+      end
+
       it "activates Melee weather effects for Biting Frost" do
         expect{ side_one.card_plays.create(card: frost) }.to change{ side_one.melee_row.weather_active? }.from(false).to(true)
       end
@@ -36,9 +65,9 @@ RSpec.describe CardPlay, type: :model do
       it "deactivates all weather effects for Clear Weather" do
         side_one.board_rows.update(weather_active: true)
         expect{ side_one.card_plays.create(card: clear) }.to change{
-          side_one.melee_row.reload.weather_active? &&
-          side_one.ranged_row.reload.weather_active? &&
-          side_one.siege_row.reload.weather_active?
+          melee_row.reload.weather_active? &&
+          ranged_row.reload.weather_active? &&
+          siege_row.reload.weather_active?
         }.from(true).to(false)
       end
     end
